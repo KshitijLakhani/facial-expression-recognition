cfea525c87855a557ebcde1930c5b8534baf6f5d
ShreenathIyer <shreenath50@gmail.com>
2018-11-18 12:19:01 -0800

    Pep8 violations

--- dataprocessing.py
+++ dataprocessing.py
@@ -25,11 +25,11 @@ def flatten_matrix(matrix):
 
 
 def zca_whitening(inputs):
-    sigma = np.dot(inputs, inputs.T)/inputs.shape[1] #Correlation matrix
-    U,S,V = np.linalg.svd(sigma) #Singular Value Decomposition
-    epsilon = 0.1                #Whitening constant, it prevents division by zero
-    ZCAMatrix = np.dot(np.dot(U, np.diag(1.0/np.sqrt(np.diag(S) + epsilon))), U.T)                     #ZCA Whitening matrix
-    return np.dot(ZCAMatrix, inputs)   #Data whitening
+    sigma = np.dot(inputs, inputs.T)/inputs.shape[1]  # Correlation matrix
+    U,S,V = np.linalg.svd(sigma)  # Singular Value Decomposition
+    epsilon = 0.1  # Whitening constant, it prevents division by zero
+    ZCAMatrix = np.dot(np.dot(U, np.diag(1.0/np.sqrt(np.diag(S) + epsilon))), U.T)  # ZCA Whitening matrix
+    return np.dot(ZCAMatrix, inputs)  # Data whitening
 
 
 def global_contrast_normalize(X, scale=1., subtract_mean=True, use_std=True,
@@ -65,9 +65,12 @@ def global_contrast_normalize(X, scale=1., subtract_mean=True, use_std=True,
     normalizers[normalizers < min_divisor] = 1.
     X /= normalizers[:, numpy.newaxis]  # Does not make a copy.
     return X
+
+
 def ZeroCenter(data):
     data = data - numpy.mean(data,axis=0)
     return data
+
 
 def normalize(arr):
     for i in range(3):
@@ -78,9 +81,11 @@ def normalize(arr):
             arr[...,i] *= (255.0/(maxval-minval))
     return arr
 
+
 def Flip(data):
     dataFlipped = data[..., ::-1].reshape(2304).tolist()
     return dataFlipped
+
 
 def Roated15Left(data):
     num_rows, num_cols = data.shape[:2]
@@ -88,11 +93,13 @@ def Roated15Left(data):
     img_rotation = cv2.warpAffine(data, rotation_matrix, (num_cols, num_rows))
     return img_rotation.reshape(2304).tolist()
 
+
 def Roated15Right(data):
     num_rows, num_cols = data.shape[:2]
     rotation_matrix = cv2.getRotationMatrix2D((num_cols/2, num_rows/2), -30, 1)
     img_rotation = cv2.warpAffine(data, rotation_matrix, (num_cols, num_rows))
     return img_rotation.reshape(2304).tolist()
+
 
 def Zoomed(data):
     datazoomed = scipy.misc.imresize(data,(60,60))
@@ -100,28 +107,36 @@ def Zoomed(data):
     datazoomed = datazoomed.reshape(2304).tolist()
     return datazoomed
 
+
 def shiftedUp20(data):
     translated = imutils.translate(data, 0, -5)
     translated2 = translated.reshape(2304).tolist()
     return translated2
+
+
 def shiftedDown20(data):
     translated = imutils.translate(data, 0, 5)
     translated2 = translated.reshape(2304).tolist()
     return translated2
 
+
 def shiftedLeft20(data):
     translated = imutils.translate(data, -5, 0)
     translated2 = translated.reshape(2304).tolist()
     return translated2
+
+
 def shiftedRight20(data):
     translated = imutils.translate(data, 5, 0)
     translated2 = translated.reshape(2304).tolist()
     return translated2
 
+
 def outputImage(pixels,number):
     data = pixels
     name = str(number)+"output.jpg" 
     scipy.misc.imsave(name, data)
+
 
 def Zerocenter_ZCA_whitening_Global_Contrast_Normalize(list):
     Intonumpyarray = numpy.asarray(list)
@@ -131,6 +146,7 @@ def Zerocenter_ZCA_whitening_Global_Contrast_Normalize(list):
     data4 = global_contrast_normalize(data3)
     data5 = numpy.rot90(data4,3)
     return data5
+
 
 def load_test_data():
     f = open('fer2013.csv')
@@ -146,6 +162,7 @@ def load_test_data():
             data = Zerocenter_ZCA_whitening_Global_Contrast_Normalize(temp_list)
             test_set_x.append(data)
     return test_set_x, test_set_y
+
 
 def load_data():
 
@@ -164,7 +181,7 @@ def load_data():
     csv_f = csv.reader(f)
 
     for row in csv_f:   
-        number+= 1
+        number += 1
         if number not in ToBeRemovedTrainingData:
 
             if str(row[2]) == "Training" or str(row[2]) == "PublicTest" :
