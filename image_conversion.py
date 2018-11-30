992b90105efedc1990a159fddc1ba258caf27d2d
ShreenathIyer <shreenath50@gmail.com>
2018-11-30 11:42:48 -0800

    Write image to csv file

--- image_conversion.py
+++ image_conversion.py
@@ -6,10 +6,8 @@ photos_base_path = os.path.abspath(os.path.join(os.path.dirname(__file__) + '/ph
 
 for subdir, dirs, files in os.walk(photos_base_path):
     for file in files:
-        # print(os.path.join(subdir, file))
-        im = Image.open(os.path.join(subdir, file))
+        im = Image.open(os.path.join(subdir, file)).convert('1')
         pixels = list(im.getdata())
         width, height = im.size
         pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
-        print(pixels)
-
+        np.savetxt("pixel_data.csv", pixels, delimiter=",")
