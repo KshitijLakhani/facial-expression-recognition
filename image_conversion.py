1faf4623ea6978cf1c2aacc66c18c1123820b417
ShreenathIyer <shreenath50@gmail.com>
2018-11-30 11:49:50 -0800

    Change RGB values to grayscale before writing to csv

--- image_conversion.py
+++ image_conversion.py
@@ -6,8 +6,8 @@ photos_base_path = os.path.abspath(os.path.join(os.path.dirname(__file__) + '/ph
 
 for subdir, dirs, files in os.walk(photos_base_path):
     for file in files:
-        im = Image.open(os.path.join(subdir, file)).convert('1')
+        im = Image.open(os.path.join(subdir, file)).convert('L')
         pixels = list(im.getdata())
         width, height = im.size
         pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
-        np.savetxt("pixel_data.csv", pixels, delimiter=",")
+        np.savetxt("pixel_data.csv", pixels, fmt='%d', delimiter=",")
