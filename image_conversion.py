d737e385797d522d6b54a678006d0b2430847fbe
ShreenathIyer <shreenath50@gmail.com>
2018-11-30 12:27:54 -0800

    Refactor surprise file and update image-conversion with emotion values

--- image_conversion.py
+++ image_conversion.py
@@ -1,6 +1,7 @@
 import os
-from PIL import Image
 import numpy as np
+import csv
+from PIL import Image
 
 photos_base_path = os.path.abspath(os.path.join(os.path.dirname(__file__) + '/photos/'))
 data_kind = "PrivateTest"
@@ -11,15 +12,23 @@ emotion_mappings = {'angry': '0',
                     'sad': '4',
                     'surprise': '5',
                     'neutral': '6'}
-
+to_write = list()
 
 for subdir, dirs, files in os.walk(photos_base_path):
     for file in files:
-        curr_emotion = file.split('-')[0]
+        final_list = list()
+        curr_emotion = emotion_mappings[str(file.split('-')[0])]
         im = Image.open(os.path.join(subdir, file)).convert('L')
         pixels = list(im.getdata())
         width, height = im.size
         pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
         pixels = np.asarray(pixels).flatten().tolist()
-        img_str = ' '.join(pixels)
-        np.savetxt("test_data.csv", pixels, fmt='%d', delimiter=",")
+        img_str = ' '.join(map(str, pixels))
+        final_list.append(curr_emotion)
+        final_list.append(img_str)
+        final_list.append(data_kind)
+        to_write.append(final_list)
+
+with open("test_data.csv", 'w') as outfile:
+    writer = csv.writer(outfile)
+    writer.writerows(to_write)
