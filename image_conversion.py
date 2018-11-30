00023fea05a859ffa9085a472e256eb3ef669120
ShreenathIyer <shreenath50@gmail.com>
2018-11-30 12:07:00 -0800

    Update with emotion type and data kind before writing to csv

--- image_conversion.py
+++ image_conversion.py
@@ -3,11 +3,23 @@ from PIL import Image
 import numpy as np
 
 photos_base_path = os.path.abspath(os.path.join(os.path.dirname(__file__) + '/photos/'))
+data_kind = "PrivateTest"
+emotion_mappings = {'angry': '0',
+                    'disgust': '1',
+                    'fear': '2',
+                    'happy': '3',
+                    'sad': '4',
+                    'surprise': '5',
+                    'neutral': '6'}
+
 
 for subdir, dirs, files in os.walk(photos_base_path):
     for file in files:
+        curr_emotion = file.split('-')[0]
         im = Image.open(os.path.join(subdir, file)).convert('L')
         pixels = list(im.getdata())
         width, height = im.size
         pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
-        np.savetxt("pixel_data.csv", pixels, fmt='%d', delimiter=",")
+        pixels = np.asarray(pixels).flatten().tolist()
+        img_str = ' '.join(pixels)
+        np.savetxt("test_data.csv", pixels, fmt='%d', delimiter=",")
